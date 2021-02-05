aa54bc887b820cb4b7e87f2b6d921aaa92ea40ca
David <71188118+davidmelash@users.noreply.github.com>
2021-02-05 13:29:09 +0200

    Add files via upload

--- scraping_price.py
+++ scraping_price.py
@@ -1,15 +1,17 @@
+import re
 from bs4 import BeautifulSoup
 import urllib.request
 
-result = []
+results = []
+PATH = 'https://www.olx.ua/uk/elektronika/telefony-i-aksesuary/q-google-pixel-3/?page='
 
 
 def get_content(page=0, number_of_pages=1):
     """Scraping our site"""
     
-    while page != number_of_pages:                 # number of pages
+    while page != number_of_pages:        # number of pages
         try:
-            r = urllib.request.urlopen(f'https://www.olx.ua/uk/elektronika/telefony-i-aksesuary/q-google-pixel-3/?page={page}')
+            r = urllib.request.urlopen(f'{PATH}{page}')
             html = r.read()
             page += 1
             soup = BeautifulSoup(html, 'html.parser')
@@ -19,13 +21,11 @@ def get_content(page=0, number_of_pages=1):
             break
 
 
-def select_price(array):
-    """Replace useless characters"""
+def select_price(price):
+    """Filter by price"""
+    
+    return True if 2000 < int(re.sub('[ ,грн.]', '', price)) < 5000 else False
 
-    try:
-        return True if 2000 < int(array.replace('грн.', ' ').replace(' ', '')) < 5000 else False
-    except ValueError:
-        pass
 
 
 def select_content(new):
@@ -35,10 +35,11 @@ def select_content(new):
         price = item.find("p", class_="price").get_text(strip=True)
         title = item.find("strong").get_text(strip=True)
         href = item.a.get("href")
+        print(price)
         if select_price(price):
-            result.append([title, price, href])
-    s = sorted(result, key=lambda x: x[1], reverse=True)
-    write_down(s)
+            results.append([title, price, href])
+    result = sorted(results, key=lambda x: x[1], reverse=True)
+    write_down(result)
 
 
 def write_down(result):
@@ -47,7 +48,7 @@ def write_down(result):
     with open("price.txt", "w", encoding="utf-8") as file:
         for i in result:
             file.write(f'Title : {i[0]}\nPrice : {i[1]}\nLink : {i[2]}\n\n')
-            
+
 
 if __name__ == '__main__':
-    get_content(0, 1)
+    get_content(0, 2)
